3a7cd0b028753ec544a5b7899897d3da35e6ac43
yulia-exe <93983598+yulia-exe@users.noreply.github.com>
2021-11-09 15:47:37 +0300

    Add files via upload

---
+++ Кицкан Полина/задача на проценты.py
@@ -0,0 +1,10 @@
+print( 'введите капитал ')
+a=int(input())
+print( 'введите кол-во дней ')
+b=int(input())
+print("введите проценты")
+c=int(input())
+c=c/100
+k=a*c*b
+print("капитал уве-ся за 360 дней на:")
+print(k)
