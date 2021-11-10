61b075e289be530ae7e65ef04ca8e1ef89b2c8c2
ZhenyaKrutoy <93984099+ZhenyaKrutoy@users.noreply.github.com>
2021-11-10 18:23:06 +0300

    Качанов Е. 9а

---
+++ задание 2.py
@@ -0,0 +1,11 @@
+import math
+a = float(input())
+b = float(input())
+c = float(input())
+m = float(input())
+n = float(input())
+p = float(input())
+n = (a+b+c)/3
+f = n +(m+n+p)
+print('Цена смеси за ОДИН фунт:')
+print(f)
