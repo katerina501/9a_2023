0b7c3847eb8beafbe06b23bb038e524e6f5a1616
yulia-exe <93983598+yulia-exe@users.noreply.github.com>
2021-11-30 15:07:35 +0300

    Add files via upload

---
+++ Кицкан Полина/logvar1.py
@@ -0,0 +1,6 @@
+a = int(input('введите число'))
+b= (a>99)*(a<1000)
+if not b:
+    print ('no')
+else:
+    print ('yes')
---
+++ Кицкан Полина/logvar2.py
@@ -0,0 +1,9 @@
+
+a = int(input('введите число'))
+b=a//100
+c=a%10
+a=b==c
+if not a:
+    print('no')
+else:
+    print("yes")
