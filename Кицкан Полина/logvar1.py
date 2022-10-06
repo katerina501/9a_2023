da1585fe8bb50591c4e89d664ac483033485259f
katerina501 <31926736+katerina501@users.noreply.github.com>
2022-10-06 12:06:52 +0300

    Delete Кицкан Полина directory

--- Кицкан Полина/logvar1.py
+++
@@ -1,6 +0,0 @@
-a = int(input('введите число'))
-b= (a>99)*(a<1000)
-if not b:
-    print ('no')
-else:
-    print ('yes')
--- Кицкан Полина/logvar2.py
+++
@@ -1,9 +0,0 @@
-
-a = int(input('введите число'))
-b=a//100
-c=a%10
-a=b==c
-if not a:
-    print('no')
-else:
-    print("yes")
